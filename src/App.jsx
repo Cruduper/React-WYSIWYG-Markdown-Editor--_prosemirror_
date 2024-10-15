034ac6561113fadf356777d8bf32f061e46fa579
Cruduper <briefcasemanx@gmail.com>
2024-10-15 00:01:47 -0700

    basic Vite scaffolding for app

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import LiveMarkdownEditor from './components/LiveMarkdownEditor/LiveMarkdownEditor'
+import './styles-global/App.css'
+
+
+
+function App() {
+  return (
+    <>
+      <LiveMarkdownEditor />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/LiveMarkdownEditor/LiveMarkdownEditor.jsx
@@ -0,0 +1,9 @@
+// Main Editor Component
+function LiveMarkdownEditor() {
+
+  return (
+    <p>Hello World!</p>
+  );
+}
+
+export default LiveMarkdownEditor;
